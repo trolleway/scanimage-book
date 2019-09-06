90aae4cb9ee32699a0f5f741e7c47431611bb04c
Artem Svetlov <trolleway@yandex.ru>
2019-09-06 21:08:38 +0200

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-scanimage --format tiff --batch --progress --verbose --mode Color --resolution 300
+scanimage --format tiff --batch --progress --verbose --mode Color --resolution 300 --wait-for-button yes
